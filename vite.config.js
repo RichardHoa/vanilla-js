95a80e9edf8ee046f3c5f1a3abb7dc07e6dc385b
Richard Hoa <141636214+RichardHoa@users.noreply.github.com>
2025-03-04 19:43:35 +0700

    most stable version

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,5 @@ export default defineConfig({
   build: {
     outDir: "dist",
     target: "esnext",
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Automatically create separate chunks for large modules
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-          if (id.includes("/src/Pages/")) {
-            return "pages";
-          }
-          if (id.includes("/src/Components/")) {
-            return "components";
-          }
-        },
-      },
-    },
   },
 });
